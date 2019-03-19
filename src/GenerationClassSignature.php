6c03404dc1ac6f7d4cf649b56d153e1c66b3bac7
yashuk803 <yashuk803@gmail.com>
2019-03-19 15:51:43 +0200

    add header PhpDoc

--- src/GenerationClassSignature.php
+++ src/GenerationClassSignature.php
@@ -1,11 +1,12 @@
 <?php
 
-/**
+/*
  * This file is part of the "greeflas/php-static-analyzer" package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Greeflas\StaticAnalyzer;
 
 /**
