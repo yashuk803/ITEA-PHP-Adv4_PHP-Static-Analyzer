b182810e11f29fdcbdca65781eccf409f699cad2
yashuk803 <yashuk803@gmail.com>
2019-03-19 11:41:44 +0200

    edit name project in PhpDoc in headers

--- src/Command/ClassSignatureStatCommand.php
+++ src/Command/ClassSignatureStatCommand.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the "project" package.
+ * This file is part of the "greeflas/php-static-analyzer" package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
